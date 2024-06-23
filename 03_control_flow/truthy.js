4e993a0f35eabe9a53d2064d5a3d6a0f1ed5cf23
Nishita77 <115980101+Nishita77@users.noreply.github.com>
2024-06-23 14:56:34 +0000

    truthy

---
+++ 03_control_flow/truthy.js
@@ -0,0 +1,17 @@
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {      //array checking condition
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
